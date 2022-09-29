07e73662a90f6cdf9ba22e5a28efabb36814b86e
Howard Butler <hobu.inc@gmail.com>
2022-09-29 12:55:57 -0500

    use meshio to write ply files

--- vcd/extract-cluster.py
+++ vcd/extract-cluster.py
@@ -4,23 +4,26 @@ import sys
 
 
 import argparse
-
-from scipy.spatial import Delaunay
-import matplotlib.pyplot as plt
+import sys
+from scipy.spatial import Delaunay, ConvexHull
 import numpy as np
-from mayavi import mlab
+import meshio
 
+# python vcd/extract-cluster.py clustered.laz --cluster_dimension=DBScan 2972
 
 def scale(a, bounds=200):
     return np.interp(a, (a.min(), a.max()), (-bounds, +bounds))
 
-
 def extract_cluster(args):
     pipeline = pdal.Reader(filename = args.input)
-    pipeline |= pdal.Filter.range(limits=f"{args.cluster_dimension}[{int(args.cluster_id)}:{int(args.cluster_id)}]")
+    pipeline |= pdal.Filter.range(limits=f"{args.cluster_dimension}[{args.cluster_id}:{args.cluster_id}]")
 
     e = pipeline.execute()
     arr = pipeline.arrays[0]
+
+    if not len(arr):
+        print (f"Cluster {args.cluster_id} does not have any points")
+        sys.exit(1)
 
     x = arr['X']
     y = arr['Y']
@@ -29,24 +32,21 @@ def extract_cluster(args):
 
 
     points = np.vstack((x,y,z)).T
+    print (f'computing Delaunay of {len(points)} points')
 
 
+    # https://stackoverflow.com/a/21343991/498396
     tri = Delaunay(points)
 
-    # some how plot the thing
-    fig = mlab.figure(1, bgcolor=(1, 0.7, 1), fgcolor=(0.5, 0.5, 0.5))
+    # compute the convex hull of the points
 
-    # Generate triangular Mesh:
-    tmesh = mlab.triangular_mesh(x, y, z, tri.vertices,
-                                 scalars=y, colormap='jet')
-    mlab.show()
+    meshio.write(f"{args.cluster_dimension}-{args.cluster_id}.ply", mesh=meshio.Mesh(points=points, cells = [("quad", tri.simplices)]))
 
-    import pdb;pdb.set_trace()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Extract cluster with given ID from LAZ file')
     parser.add_argument('input', help='Input file to read')
-    parser.add_argument('cluster_id', help='ID of the cluster to extract')
+    parser.add_argument('cluster_id', type=int, help='ID of the cluster to extract')
     parser.add_argument('--cluster_dimension', default="ClusterID", help='Dimension to use for selection')
     args = parser.parse_args()
 
